e252f55595b1cb51df716c6e9f740c89d22960bb
nandaAsmanuri <nanda94artzasma@gmail.com>
2023-01-16 19:58:15 +0800

    first commit

---
+++ FDA_E020320083/app/src/main/java/id/ac/poliban/mi/VC/e020320083/fda_e020320083/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.VC.e020320083.fda_e020320083.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ WisataApp_E020320083/app/src/main/java/id/ac/poliban/mi/VC/e020320083/wisataapp_e020320083/model/ModelMain.java
@@ -0,0 +1,30 @@
+package id.ac.poliban.mi.VC.e020320083.wisataapp_e020320083.model;
+
+
+public class ModelMain {
+
+    private String txtName;
+    private int imgSrc;
+
+    public ModelMain(String txtName, int imgSrc) {
+        this.txtName = txtName;
+        this.imgSrc = imgSrc;
+    }
+
+    public String getTxtName() {
+        return txtName;
+    }
+
+    public void setTxtName(String txtName) {
+        this.txtName = txtName;
+    }
+
+    public int getImgSrc() {
+        return imgSrc;
+    }
+
+    public void setImgSrc(int imgSrc) {
+        this.imgSrc = imgSrc;
+    }
+
+}
